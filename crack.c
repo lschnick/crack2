f64b8849dd4f7a2db928a46eacc5d7486d05ead7
lschnick <lschnick@student.sierracollege.edu>
2025-05-04 14:07:17 -0700

    Add files via upload
    First commit, git keeps asking for username and password

--- crack.c
+++ crack.c
@@ -13,11 +13,36 @@ const int HASH_LEN = 33;        // Length of MD5 hash strings
 // Get this function working first!
 char * tryWord(char * plaintext, char * hashFilename)
 {
+    int size = 0;
+    char ln[HASH_LEN];
+    size = strlen(plaintext);
     // Hash the plaintext
-
+    char *hashed = md5(plaintext, size);
     // Open the hash file
-
+    FILE *filer = fopen(hashFilename, "r");
+    if (filer == NULL)
+    {
+        printf("error with hash file\n");
+        exit(1);
+    }
     // Loop through the hash file, one line at a time.
+    while ((fgets(ln, HASH_LEN, filer))!= NULL)
+    {
+        int length = strlen(ln);
+        int i = length - 1;
+        if ( ln[i] == '\n')
+        {
+            ln[i] = '\0';
+        }
+        
+        if ((strstr(ln, hashed)) != NULL)
+        {
+            return hashed;
+        }
+    }
+    return NULL;
+    fclose(filer);
+    free(hashed);
 
     // Attempt to match the hash from the file to the
     // hash of the plaintext.
@@ -28,10 +53,6 @@ char * tryWord(char * plaintext, char * hashFilename)
     // Before returning, do any needed cleanup:
     //   Close files?
     //   Free memory?
-
-    // Modify this line so it returns the hash
-    // that was found, or NULL if not found.
-    return "0123456789abcdef0123456789abcdef";
 }
 
 
@@ -43,26 +64,46 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    // These two lines exist for testing. When you have
-    // tryWord working, it should display the hash for "hello",
-    // which is 5d41402abc4b2a76b9719d911017c592.
-    // Then you can remove these two lines and complete the rest
-    // of the main function below.
-    char *found = tryWord("hello", "hashes00.txt");
-    printf("%s %s\n", found, "hello");
-
-
-    // Open the dictionary file for reading.
     
+    char dictln[PASS_LEN];
+    // Open the dictionary file for reading.
+    FILE *dictf = fopen(argv[2], "r");
+     if (dictf == NULL)
+    {
+        printf("error with dict file");
+        exit(1);
+    }
+    int count;
+    int size = 0;
 
-    // For each dictionary word, pass it to tryWord, which
+    while ((fgets(dictln, PASS_LEN, dictf))!= NULL)
+    {
+        size = strlen(dictln);
+        int z = size - 1;
+        if ( dictln[z] == '\n')
+        {
+            dictln[z] = '\0';
+        }
+
+        char *hashstore = (tryWord(dictln, argv[1]));
+        if (hashstore != NULL) 
+        {
+            fputs(hashstore, stdout);
+            printf(": %s\n", dictln);
+            count++;
+            free(hashstore);
+        }
+    }
+    printf("Cracked %d hashes\n", count);
+
+    // For each dictionary word, pass it to tryWord, whichS
     // will attempt to match it against the hashes in the hash_file.
     
     // If we got a match, display the hash and the word. For example:
     //   5d41402abc4b2a76b9719d911017c592 hello
     
     // Close the dictionary file.
-
+    fclose(dictf);
     // Display the number of hashes that were cracked.
     
     // Free up any malloc'd memory?
